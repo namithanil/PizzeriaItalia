2fe000d621f119f14500cf9f279a9c0615994fd2
Namith Anil <93058815+namithanil@users.noreply.github.com>
2021-10-23 18:43:27 -0400

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,86 @@
+<?php
+#this is Login form page , if user is already logged in then we will not allow user to access this page by executing isset($_SESSION["uid"])
+#if below statment return true then we will send user to their profile.php page
+if (isset($_SESSION["uid"])) {
+	header("location:profile.php");
+}
+//in action.php page if user click on "ready to checkout" button that time we will pass data in a form from action.php page
+if (isset($_POST["login_user_with_product"])) {
+	//this is product list array
+	$product_list = $_POST["product_id"];
+	//here we are converting array into json format because array cannot be store in cookie
+	$json_e = json_encode($product_list);
+	//here we are creating cookie and name of cookie is product_list
+	setcookie("product_list",$json_e,strtotime("+1 day"),"/","","",TRUE);
+
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Pizzeria Italia</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">Pizzeria Italia</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+				<li><a href="login/login.php"><span class="glyphicon glyphicon-home"></span> Lost and Found Directory</a></li>
+				<li><a href="admin"><span class="glyphicon glyphicon-home"></span> Admin Login</a></li>
+				<li><a href="aboutus.php"><span class="glyphicon glyphicon-home"></span> About Us</a></li>
+			</ul>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="signup_msg">
+				<!--Alert from signup form-->
+		</div>
+		<h1>
+           <b>&emsp;  About Us</b>  
+		   <br>
+		   <br>
+		   </h1>
+		   <h4>
+		   &emsp; &emsp; We are a restaurant 
+           </h4>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
